03a3e2a1a8a7bb4b9a810c06108d37cfe2129564
Taihao Zhang <jason@zth.me>
2011-03-30 13:33:14 +1100

    Render tables separately (independently of the table style) and added option for hiding empty fields.

--- views-view-two-column-table.tpl.php
+++ views-view-two-column-table.tpl.php
@@ -6,7 +6,8 @@
  *
  * - $title : The title of this group of rows.  May be empty.
  * - $class : A class or classes to apply to the table, based on settings.
- * - $row_classes: An array of classes to apply to each row, indexed by row
+ * - $row_classes: An array of classes to apply to each table, each row,
+ *   indexed by table then by row.
  * - $header: An array of header labels keyed by field id.
  * - $fields: An array of CSS IDs to use for each field id.
  *   number. This matches the index in $rows.
@@ -17,27 +18,25 @@
 ?>
 
 
-<table class="<?php print $class; ?>">
+<div class="<?php print $class; ?>">
   <?php if (!empty($title)) : ?>
-    <caption><?php print $title; ?></caption>
+    <h3><?php print $title; ?></h3>
   <?php endif; ?>
-  <tbody>
-    <?php foreach ($rows as $count => $row): ?>
-      <tr class="<?php print implode(' ', $row_classes[$count]); ?>">
-        <table class="views-row-table"><tbody>
-          <?php foreach ($row as $field => $content): ?>
-            <tr class="views-field-row views-field-row-<?php print $fields[$field]; ?>">
-              <th class="views-field-label views-field-label-<?php print $fields[$field]; ?>">
-                <?php print $header[$field]; ?>
-              </th>
-              <td class="views-field views-field-<?php print $fields[$field]; ?>">
-                <?php print $content; ?>
-              </td>
-            </tr>
-          <?php endforeach; ?>
-        </tbody></table>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <?php foreach ($rows as $count => $row): ?>
+    <table class="views-row-table">
+      <tbody>
+        <?php foreach ($row as $field => $content): ?>
+          <tr class="views-field-row views-field-row-<?php print $fields[$field]; ?> <?php print implode(' ', $row_classes[$count][$field]); ?>">
+            <th class="views-field-label views-field-label-<?php print $fields[$field]; ?>">
+              <?php print $header[$field]; ?>
+            </th>
+            <td class="views-field views-field-<?php print $fields[$field]; ?>">
+              <?php print $content; ?>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php endforeach; ?>
+</div>
 
